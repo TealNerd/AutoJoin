61d9e37ca0340926e13e3429581b8d91d6baf8d9
TealNerd <jacobsokora@gmail.com>
2015-08-25 13:04:13 -0500

    Added macromod auto relog
    If you have a macromod global boolean called "auto_relog" set as true it
    will relog

--- src/main/java/com/biggestnerd/autojoin/AutoJoin.java
+++ src/main/java/com/biggestnerd/autojoin/AutoJoin.java
@@ -1,15 +1,22 @@
 package com.biggestnerd.autojoin;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiDisconnected;
+import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraft.client.multiplayer.GuiConnecting;
 import net.minecraft.client.multiplayer.ServerData;
+import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -17,7 +24,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
 
-@Mod(modid="autojoin", name="AutoJoin", version="v1.3.1")
+@Mod(modid="autojoin", name="AutoJoin", version="v1.4")
 public class AutoJoin {
 
 	Minecraft mc = Minecraft.getMinecraft();
@@ -26,6 +33,10 @@ public class AutoJoin {
 	boolean logoutFlag;
 	File logFile;
 	SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+	boolean macromod = false;
+	File macroFolder;
+	File macroGlobalVars;
+	long logoutCheckTimer = 0;
 	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
@@ -36,6 +47,13 @@ public class AutoJoin {
 				logFile.createNewFile();
 			} catch (Exception ex) {
 				ex.printStackTrace();
+			}
+		}
+		macroFolder = new File(mc.mcDataDir, "liteconfig/common/macros");
+		if(macroFolder.isDirectory()) {
+			macroGlobalVars = new File(macroFolder, ".globalvars.xml");
+			if(macroGlobalVars.exists()) {
+				macromod = true;
 			}
 		}
 	}
@@ -63,10 +81,40 @@ public class AutoJoin {
 			logoutFlag = true;
 		}
 		if(counter >= 420) {
-			mc.displayGuiScreen(new GuiConnecting(mc.currentScreen, mc, last));
+			mc.displayGuiScreen(new GuiConnecting(new GuiMainMenu(), mc, last));
 			System.out.println("Connecting to " + last.serverIP);
 			counter = 0;
 		}
+		if(mc.theWorld != null && !mc.isSingleplayer() && System.currentTimeMillis() - logoutCheckTimer > 5000) {
+			logoutCheckTimer = System.currentTimeMillis();
+			try {
+				if(isAutoRelog()) {
+					last = mc.getCurrentServerData();
+					mc.theWorld.sendQuittingDisconnectingPacket();
+					mc.displayGuiScreen(new GuiDisconnected(null, "Get fukt", new ChatComponentText("lelelelele")));
+				}
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+	
+	public boolean isAutoRelog() throws Exception {
+		if(!macromod) {
+			return false;
+		}
+		Pattern p = Pattern.compile("auto_relog\">(0|1)");
+		BufferedReader reader = new BufferedReader(new FileReader(macroGlobalVars));
+		String line = "";
+		while((line = reader.readLine()) != null) {
+			Matcher m = p.matcher(line);
+			if(m.find()) {
+				reader.close();
+				return Integer.parseInt(m.group(1)) == 1;
+			}
+		}
+		reader.close();
+		return false;
 	}
 	
 	public void logToFile(String message) throws Exception {
