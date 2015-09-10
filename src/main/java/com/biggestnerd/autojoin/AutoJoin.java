9a3aefab8a01d887a94ac5ba538e8510bcdc6ba2
biggestnerd <TealNerd@users.noreply.github.com>
2015-09-10 02:00:59 -0500

    Added comments for Sanwi and probably fixed auto-relog bug

--- src/main/java/com/biggestnerd/autojoin/AutoJoin.java
+++ src/main/java/com/biggestnerd/autojoin/AutoJoin.java
@@ -5,9 +5,10 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,7 +17,6 @@ import net.minecraft.client.gui.GuiDisconnected;
 import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraft.client.multiplayer.GuiConnecting;
 import net.minecraft.client.multiplayer.ServerData;
-import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -61,14 +61,17 @@ public class AutoJoin {
 		}
 	}
 	
+	//fires every tick, 30 times per second?
 	@SubscribeEvent
 	public void onTick(ClientTickEvent event) {
+		//if we're looking at minecraft's disconnected/kicked by server screen
 		if(mc.currentScreen instanceof GuiDisconnected) {
 			if(logoutFlag) {
 				Date date = new Date();
 				String log = "[" + (System.currentTimeMillis() / 1000L) + "][" + format.format(date) + "] Disconnected";
 				logoutFlag = false;
 			}
+			//increments counter if it's less than 420
 			if(counter <= 420) {
 				counter++;
 			}
@@ -83,14 +86,20 @@ public class AutoJoin {
 			mc.setIngameFocus();
 			logoutFlag = true;
 		}
+		//if counter is >= 420 then it's been 14 seconds since we were disconnected, and we should try logging in again
 		if(counter >= 420) {
 			mc.displayGuiScreen(new GuiConnecting(new GuiMainMenu(), mc, last));
 			System.out.println("Connecting to " + last.serverIP);
+			//reset the counter for next time
 			counter = 0;
 		}
+		//checks if we're ingame, then checks if it's been 5 seconds since we last checked for the global logout variable
+		//calculates it by saying currentTime - lastCheckTime > 5 seconds
 		if(mc.theWorld != null && !mc.isSingleplayer() && System.currentTimeMillis() - logoutCheckTimer > 5000) {
+			//resets check timer
 			logoutCheckTimer = System.currentTimeMillis();
 			try {
+				//getAutoRelogTime gives us a time in milliseconds for how long we should stay logged out, it will be 0 if we shouldnt log out
 				if(getAutoRelogTime() != 0) {
 					last = mc.getCurrentServerData();
 					mc.theWorld.sendQuittingDisconnectingPacket();
@@ -98,11 +107,13 @@ public class AutoJoin {
 					relogTime = getAutoRelogTime();
 					relogStartTime = System.currentTimeMillis();
 					relog = true;
+					resetRelogVariable();
 				}
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
 		}
+		//if we're supposed to relog, and we're not ingame, and it's been relogtime since we logged out, log back in and set relog to false
 		if(relog && !mc.inGameHasFocus && System.currentTimeMillis() - relogStartTime > relogTime) {
 			mc.displayGuiScreen(new GuiConnecting(new GuiMainMenu(), mc, last));
 			relog = false;
@@ -127,8 +138,29 @@ public class AutoJoin {
 		return 0;
 	}
 	
+	public void resetRelogVariable() throws Exception {
+		Pattern p = Pattern.compile("#auto_relog\">(\\d+)");
+		BufferedReader reader = new BufferedReader(new FileReader(macroGlobalVars));
+		List<String> lines = new ArrayList<String>();
+		String line = "";
+		while((line = reader.readLine()) != null) {
+			Matcher m = p.matcher(line);
+			if(m.find()) {
+				lines.add("    <entry key=\"#auto_relog\">0</entry>");
+			} else {
+				lines.add(line);
+			}
+		}
+		reader.close();
+		BufferedWriter writer = new BufferedWriter(new FileWriter(macroGlobalVars));
+		for(String s : lines) {
+			writer.write(s + "\n");
+		}
+		writer.close();
+	}
+	
 	public void logToFile(String message) throws Exception {
-		BufferedWriter writer = new BufferedWriter(new FileWriter(logFile));
+		BufferedWriter writer = new BufferedWriter(new FileWriter(logFile, true));
 		writer.write(message);
 		writer.close();
 	}
